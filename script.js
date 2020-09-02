d85746a0d2a682651b3ed9e69cc8c9a2d02bf004
Usama Umar <usamaumar118eb@gmail.com>
2020-09-02 12:33:35 -0700

    added script and set interval to call the function

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('here')
+const daysEl = document.getElementById('days')
+const hoursEl = document.getElementById('hours')
+const minsEl = document.getElementById('mins')
+const secsEl = document.getElementById('secs')
+
+const newYear = "1 Jan 2021"
+
+
+function countdown(){
+
+    const remainingTime = new Date(newYear) 
+    const currentDate = new Date
+
+    const totalsecs = (remainingTime - currentDate) / 1000
+
+    const days = Math.floor(totalsecs / 3600 / 24 ) 
+    const hours = Math.floor(totalsecs / 3600) % 24
+    const mins = Math.floor(totalsecs / 60) % 60
+    const secs = Math.floor(totalsecs) % 60
+
+    daysEl.innerHTML = days
+    hoursEl.innerHTML = hours
+    minsEl.innerHTML = mins
+    secsEl.innerHTML = secs
+}
+
+
+setInterval(countdown, 1000)
